ffb1ec116db1d8db32cf61d4943c598863ff4ee7
Tean Lai <laitean19@gmail.com>
2024-06-12 01:34:24 -0400

    Added linting and formatting on save to neovim config

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,48 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier" } },
+        graphql = { { "prettierd", "prettier" } },
+        java = { "google-java-format" },
+        kotlin = { "ktlint" },
+        ruby = { "standardrb" },
+        markdown = { { "prettierd", "prettier" } },
+        erb = { "htmlbeautifier" },
+        html = { "htmlbeautifier" },
+        bash = { "beautysh" },
+        proto = { "buf" },
+        rust = { "rustfmt" },
+        yaml = { "yamlfix" },
+        toml = { "taplo" },
+        css = { { "prettierd", "prettier" } },
+        scss = { { "prettierd", "prettier" } },
+        cpp = { "clang-format" }
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+
+
+
+
+    vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+
+}
