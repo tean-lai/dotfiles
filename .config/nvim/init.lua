8a38190653cd3d76f69e45126725696e323b727e
Tean Lai <laitean19@gmail.com>
2024-06-13 01:20:31 -0400

    zsh starship

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,19 +21,19 @@ vim.opt.undofile = true
 
 vim.g.mapleader = " "
 
+vim.api.nvim_set_keymap("n", "<leader>s", ":silent !$SHELL<CR>", { noremap = true, silent = true })
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-
-
---
+++ .zshrc
@@ -0,0 +1,21 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.zsh_history
+HISTSIZE=1000
+SAVEHIST=1000
+setopt autocd extendedglob nomatch
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/tean/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+export EDITOR=nvim
+export VISUAL=nvim
+
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_FIND_NO_DUPS
+
+eval "$(starship init zsh)"
